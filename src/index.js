78cf6c985e4b0a43e1e2ea0596cdaa23cd16d665
Rafael Lagunas <rafael@imviatori.com>
2017-12-01 12:15:54 -0500

    prettier

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import { View, ScrollView} from 'react-native';
-import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
-import {Header,Button,Avatar,SocialIcon,FormLabel,Text,Divider,FormInput,Card,ListItem
-} from 'react-native-elements';
+import React from "react";
+import { View, ScrollView } from "react-native";
+import { StackNavigator } from "react-navigation"; // 1.0.0-beta.14
+import {
+  Header,
+  Button,
+  Avatar,
+  SocialIcon,
+  FormLabel,
+  Text,
+  Divider,
+  FormInput,
+  Card,
+  ListItem
+} from "react-native-elements";
 
-import Home from './components/home';
-import Details from './components/details';
+import Home from "./components/home";
+import Details from "./components/details";
 
-const HomeScreen = ({ navigation }) => (
-  <Home />
-);
+const HomeScreen = ({ navigation }) => <Home />;
 
-const DetailsScreen = () => (
-  <Details />
-);
+const DetailsScreen = () => <Details />;
 
 const App = StackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-      headerTitle: 'Home',
-    },
+      headerTitle: "Home"
+    }
   },
   Details: {
     screen: DetailsScreen,
     navigationOptions: {
-      headerTitle: 'Details',
-    },
-  },
+      headerTitle: "Details"
+    }
+  }
 });
 
 export default App;
